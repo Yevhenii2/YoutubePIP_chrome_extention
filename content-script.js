7d353238b32424e80a6e3aa49ab5dc7bbadb5999
unknown <shlapak.yevhenii@gmail.com>
2022-08-24 18:00:07 +0300

    Main

---
+++ content-script.js
@@ -0,0 +1,39 @@
+const button = createElementFromHTML(`
+  <button
+    class="ytp-button"
+    data-tooltip-target-id="ytp-autonav-toggle-button"
+    style=""
+    aria-label="Автовоспроизведение включено"
+    title="Автовоспроизведение включено"
+  >
+    <svg
+      class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium  css-w2bhrx"
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 36 36"
+      data-testid="PictureInPictureIcon"
+      aria-label="fontSize medium"
+      fill="#fff"
+    >
+      <path d="M 23 11 h -8 v 6 h 8 V 11 z m 2 -4 H 7 c -1.1 0 -2 0.9 -2 2 v 14 c 0 1.1 0.9 1.98 2 1.98 h 18 c 1.1 0 2 -0.88 2 -1.98 V 9 c 0 -1.1 -0.9 -2 -2 -2 z m 0 16.01 H 7 V 8.98 h 18 v 14.03 z"></path>
+    </svg>
+  </button>`);
+
+const buttonsContainer = document.querySelector('.ytp-right-controls');
+
+const videoTag = document.querySelector('.video-stream.html5-main-video');
+
+if ('pictureInPictureEnabled' in document) {
+  button.addEventListener('click', () => {
+    videoTag.requestPictureInPicture().catch(console.log);
+  });
+  buttonsContainer.prepend(button);
+}
+
+function createElementFromHTML(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div.firstChild;
+}
